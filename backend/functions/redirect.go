3c0f7c27ea60450f616d263e4f6c9af6933f41c8
samyfodil <fodil.samy@gmail.com>
2023-01-04 11:22:15 -0600

    First commit

---
+++ backend/functions/redirect.go
@@ -0,0 +1,10 @@
+package lib
+
+import (
+	"github.com/taubyte/go-sdk/event"
+)
+
+//export redirect
+func redirect(e event.Event) uint32 {
+	return 0
+}
---
+++ backend/functions/shorten.go
@@ -0,0 +1,10 @@
+package lib
+
+import (
+	"github.com/taubyte/go-sdk/event"
+)
+
+//export shorten
+func shorten(e event.Event) uint32 {
+	return 0
+}
